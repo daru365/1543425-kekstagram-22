92c26aba6ac8c712f12cf169b93ab79f2e4b9c8c
daru365 <70891795+daru365@users.noreply.github.com>
2021-02-19 20:25:48 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//1. Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+//2. Функция для проверки максимальной длины строки. 
+
+function lengthCounter(checkLine, maxLength) {
+	if {
+		maxLength <= 140;
+		checkLine = true; 
+	} else {
+		checkLine = false;
+	}
+	return checkLine;
+}
