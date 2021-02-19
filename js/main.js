139b8e8b4909d9d0f21d0c08692d5b8877edce9a
daru365 <70891795+daru365@users.noreply.github.com>
2021-02-19 21:00:10 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -11,11 +11,10 @@ function getRandomIntInclusive(min, max) {
 //2. Функция для проверки максимальной длины строки. 
 
 function lengthCounter(checkLine, maxLength) {
-	if {
-		maxLength <= 140;
-		checkLine = true; 
-	} else {
-		checkLine = false;
-	}
-	return checkLine;
-}
+  if (maxLength <= 140) {
+	  checkLine = true; 
+  } else {
+	  checkLine = false;
+  };
+  return checkLine;
+};
