0b955fc2b6f8ad25d3859e41516f1dec38eb8dd3
daru365 <70891795+daru365@users.noreply.github.com>
2021-02-25 18:29:08 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -5,50 +5,58 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const NUMBER = 25;
 
-getRandomIntInclusive();
-
-const idRandom = console.log(getRandomIntInclusive(1, 25));
-
-const urlRandom = console.log('photos/' + getRandomIntInclusive(1, 25) + '.jpg');
-
-const description = console.log('Описание фотографии');
-
-const likes = console.log(getRandomIntInclusive(15, 200));
-
-const messages = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-]	
+]   
 
-const names = ['Анна',
-	'Матвей',
-	'Леонид',
-	'Елена',
-	'Карл',
+const NAMES = [
+  'Анна',
+  'Матвей',
+  'Леонид',
+  'Елена',
+  'Карл',
 ]
 
-const comments = [
-	{
-	idObject: getRandomIntInclusive(),
-	avatar: 'img/avatar-' + getRandomIntInclusive(1, 6) + '.svg',
-	message: messages[_.random(0, messages.length - 1)]
-	name: names[_.random(0, names.length - 1)],
-	}
-]
- 
+const getRandomElement = (arr) => arr[getRandomIntInclusive(0, arr.length - 1)];
 
-const createObject = () => {
+const generateComments = (n) => {
+  const comment = [];
+
+  for (let i = 0; i < n; i++) {
+    comment.push({
+      id: i,
+      message: getRandomElement(MESSAGES),
+      name: getRandomElement(NAMES),
+      avatar: 'img/avatar' + getRandomIntInclusive(1, 6) +'.svg',
+    });
+  }
+
+  return comment;
+}
+
+const generateRandomObject = (id) => {
   return {
-    idRandom: '',
-    urlRandom: '',
-    description: '',
-    likes: '',
-    comments: '',
+    id,
+    url: 'photos/' + getRandomIntInclusive(1, 25) + '.jpg',
+    description: 'Описание фото',
+    comment: generateComments(getRandomIntInclusive(1, 5)),
   };
 };
 
+const generateArray = () => {
+  const data = [];
+
+  for (let i = 1; i <= NUMBER; i++) {
+    data.push(generateRandomObject(i));
+  }
+
+  return data;
+}
+generateArray();
